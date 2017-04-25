c7b9ebc332e335e8e8041ecb06b29c6b70c57a2c
bit-user <bit-user@bit42>
2017-04-25 21:12:14 +0900

    init commit

---
+++ src/prob1/Prob1.java
@@ -0,0 +1,14 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		
+		Scanner scanner = new Scanner( System.in  );
+		
+
+		scanner.close();
+ 	}
+}
---
+++ src/prob4/Prob4.java
@@ -0,0 +1,20 @@
+package prob4;
+public class Prob4 {
+
+	public static void main(String[] args) {
+		char[] c1 = reverse( "Hello World" );
+		printCharArray( c1 );
+		
+		char[] c2 = reverse( "Java Programming!" );
+		printCharArray( c2 );
+	}
+	
+	public static char[] reverse(String str) {
+		/* 코드를 완성합니다 */
+
+	}
+
+	public static void printCharArray(char[] array){
+		/* 코드를 완성합니다 */
+	}
+}
